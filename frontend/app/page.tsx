291ebe580152ba18e76f1adda7762d3aae7d0162
hewtungyuen <hewtungyuen@gmail.com>
2024-04-02 14:00:47 +0800

    initialise frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+const Home: React.FC = () => {
+  return (
+    <main className="flex min-h-screen flex-col space-y-2 items-center p-8">
+      <text className="text-5xl font-medium">Moodify</text>
+      <text className="text-darkPurple text-xl text-center">
+        Find out how your investors feel. Uncover hidden insights <br />
+        from their voices and expressions.
+      </text>
+    </main>
+  );
+};
+
+export default Home;
