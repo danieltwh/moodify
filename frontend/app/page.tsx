d985f9c605c9410ece5b3ccfaa89b3b19c64ea7d
hewtungyuen <hewtungyuen@gmail.com>
2024-04-05 23:53:20 +0800

    add upload video button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRef } from "react";
+
 const Home: React.FC = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
   return (
-    <div>
-      <text>Home</text>
+    <div className="text-center space-y-6">
+      <p className="text-darkPurple text-xl">
+        Find out how your investors feel. Uncover hidden insights
+        <br></br>
+        from their voices and expressions.
+      </p>
+      <Button onClick={() => fileInputRef.current?.click()}>
+        Upload Video
+      </Button>
+      <input
+        ref={fileInputRef}
+        type="file"
+        className="hidden"
+        onChange={(e) => console.log(e.target.files?.[0])}
+      ></input>
     </div>
   );
 };
